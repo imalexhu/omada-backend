13fb35880d3991efe01a95d1b0e5ffcd56f13f1c
PatrickS136 <tricksumarli@gmail.com>
2022-02-25 21:17:14 +1030

    datemodels v1.0

--- app/schemas/Project.js
+++ app/schemas/Project.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const enums = require("../../enum");
+// const enums = require("../../enum");
 
 const ProjectSchema = new mongoose.Schema({
   description: {
@@ -10,6 +10,25 @@ const ProjectSchema = new mongoose.Schema({
     required: true,
   },
   creator: { type: mongoose.Schema.Types.ObjectId },
+  requiredTags: { type: Map, of: Number },
+  discordLink: {
+    type: String,
+    required: true,
+  },
+  githubLink: {
+    type: String,
+    required: true,
+  },
+  difficulty: {
+    type: Number,
+  },
+  status: {
+    type: Number,
+  },
+  peopleInvolved: {
+    type: Number,
+  },
+  participants: [{ type: Map, of: Number }],
 });
 
 module.exports = mongoose.model("Project", ProjectSchema);
