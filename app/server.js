5d5f3bd7b97d289a2e158498bfdfb3ab76c5ecb6
iamalexhu <62089371+imalexhu@users.noreply.github.com>
2022-02-25 19:59:55 +1030

    Init
    Initialised database and created connection to mongo cluste

---
+++ app/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT;
+const url = process.env.DB_URL;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(
+  url,
+  () => console.log("Connected to MongoDB"),
+  (error) => console.log(error.message)
+);
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
+  
