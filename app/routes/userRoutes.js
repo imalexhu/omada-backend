4bd17f2fbbcbbc174038b036c0fca95405d483a1
iamalexhu <62089371+imalexhu@users.noreply.github.com>
2022-02-27 11:31:51 +1030

    Create user route has been set up
    Create user route works now searches database for projects with their email.
    
    Added a add user route for projects.

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -1,8 +1,19 @@
 const User = require('../schemas/User');
+const Project = require('../schemas/Project')
+
 const ENUMS = require('../../enum');
 
 async function createUser(data) {
+    let projects = await Project.find({
+        participants: data.email
+    });
+
+    console.log(projects)
+
     let user = new User(data);
+    for (projects of projects) {
+        user.currentProjectsInvolved.push(projects._id);
+    }
     await user.save();
 }
 
